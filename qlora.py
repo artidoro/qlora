175d659e904ba13837f328d31c06f1a2d0e787bd
LucianoPierdona <lucianompj1@gmail.com>
2023-06-16 17:21:56 -0300

    updates

--- qlora.py
+++ qlora.py
@@ -727,21 +727,21 @@ def train():
         class MMLUEvalCallback(transformers.TrainerCallback):
             def on_evaluate(self, args, state, control, model, **kwargs):
                 data_loader = trainer.get_eval_dataloader(mmlu_dataset)
-                source_max_len = trainer.data_collator.source_max_len
-                trainer.data_collator.source_max_len = args.mmlu_source_max_len
+                source_max_len = 359
+                trainer.data_collator.source_max_len = 359
                 trainer.model.eval()
                 preds, refs = [], []
                 loss_mmlu = 0
-                # for batch in tqdm(data_loader, total=len(data_loader)):
-                #     (loss, logits, labels) = trainer.prediction_step(trainer.model,batch,prediction_loss_only=False,)
-                #     # There are two tokens, the output, and eos token.
-                #     for i, logit in enumerate(logits):
-                #         label_non_zero_id = (batch['labels'][i] != -100).nonzero()[0][0]
-                #         logit_abcd = logit[label_non_zero_id-1][abcd_idx]
-                #         preds.append(torch.argmax(logit_abcd).item())
-                #     labels = labels[labels != IGNORE_INDEX].view(-1, 2)[:,0]
-                #     refs += [abcd_idx.index(label) for label in labels.tolist()]
-                #     loss_mmlu += loss.item()
+                for batch in tqdm(data_loader, total=len(data_loader)):
+                    (loss, logits, labels) = trainer.prediction_step(trainer.model,batch,prediction_loss_only=False,)
+                    # There are two tokens, the output, and eos token.
+                    for i, logit in enumerate(logits):
+                        label_non_zero_id = (batch['labels'][i] != -100).nonzero()[0][0]
+                        logit_abcd = logit[label_non_zero_id-1][abcd_idx]
+                        preds.append(torch.argmax(logit_abcd).item())
+                    labels = labels[labels != IGNORE_INDEX].view(-1, 2)[:,0]
+                    refs += [abcd_idx.index(label) for label in labels.tolist()]
+                    loss_mmlu += loss.item()
                 # Extract results by subject.
                 results = {'mmlu_loss':loss_mmlu/len(data_loader)}
                 subject = mmlu_dataset['subject']
