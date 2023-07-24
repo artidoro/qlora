da9155f800a2ce935a32addaa4796caee15f8184
hzg0601 <hzg0601@163.com>
2023-07-24 17:11:23 +0800

    update scripts and performance

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # ZoQLoRA: Efficient Finetuning of Quantized LLMs With ZeRO and LoRA
 
-ZeRO+Quant+LoRA
+**Performance**
+
+| method  | num_gpus | base_model    | dataset    | max_steps | train_time |
+| ------- | -------- | ------------- | ---------- | --------- | ---------- |
+| QLoRA   | 2        | huggyllama-7b | belle_0.5M | 1875      | 37h38m     |
+| QLoRA   | 1        | huggyllama-7b | belle_0.5M | 1875      | 6h43m      |
+| ZoQLoRA | 2        | huggyllama-7b | belle_0.5M | 1875      | 10h45m     |
--- scripts/finetune_guanaco_13b_zero.sh
+++ scripts/finetune_guanaco_13b_zero.sh
@@ -1,10 +1,10 @@
 nohup ds --num_gpus=2 --master_port=9901 qlora.py \
-    --model_name_or_path huggyllama/llama-30b \
-    --output_dir ./output/guanaco-33b \
+    --model_name_or_path huggyllama/llama-13b \
+    --output_dir ./output/guanaco-13b \
     --logging_steps 10 \
     --save_strategy steps \
     --data_seed 42 \
-    --save_steps 200 \
+    --save_steps 500 \
     --save_total_limit 40 \
     --evaluation_strategy steps \
     --eval_dataset_size 1024 \
@@ -24,22 +24,22 @@ nohup ds --num_gpus=2 --master_port=9901 qlora.py \
     --double_quant \
     --quant_type nf4 \
     --bf16 \
-    --bits 4 \
+    --bits 8 \
     --warmup_ratio 0.03 \
     --lr_scheduler_type constant \
     --gradient_checkpointing \
-    --dataset belle_0.5m \
+    --dataset oasst1 \
     --source_max_len 16 \
     --target_max_len 512 \
     --per_device_train_batch_size 2 \
     --gradient_accumulation_steps 16 \
     --max_steps 1875 \
     --eval_steps 187 \
-    --learning_rate 0.0001 \
+    --learning_rate 0.0002 \
     --adam_beta2 0.999 \
     --max_grad_norm 0.3 \
     --lora_dropout 0.05 \
     --weight_decay 0.0 \
     --deepspeed "deepspeed_config.json" \
     --seed 0 \
-    >guanaco_33b_zero_2.log 2>&1 &
+    >guanaco_13b_zero_2.log 2>&1 &
