b30db79262d3d42b72c753567723cb0b51307d1a
hzg0601 <hzg0601@163.com>
2023-07-24 14:04:51 +0800

    test 2gpu

---
+++ scripts/nods_fintunue_guanaco_7b_2gpu.sh
@@ -0,0 +1,44 @@
+nohup python -u  qlora.py \
+    --model_name_or_path huggyllama/llama-7b \
+    --output_dir ./output/guanaco-7b-nods_2gpu \
+    --logging_steps 10 \
+    --save_strategy steps \
+    --data_seed 42 \
+    --save_steps 500 \
+    --save_total_limit 40 \
+    --evaluation_strategy steps \
+    --eval_dataset_size 1024 \
+    --max_eval_samples 1000 \
+    --per_device_eval_batch_size 2 \
+    --max_new_tokens 32 \
+    --dataloader_num_workers 3 \
+    --group_by_length \
+    --logging_strategy steps \
+    --remove_unused_columns False \
+    --do_train \
+    --do_eval \
+    --do_mmlu_eval \
+    --lora_r 64 \
+    --lora_alpha 16 \
+    --lora_modules all \
+    --double_quant \
+    --quant_type nf4 \
+    --bf16 \
+    --bits 4 \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type constant \
+    --gradient_checkpointing \
+    --dataset belle_0.5m \
+    --source_max_len 16 \
+    --target_max_len 512 \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 16 \
+    --max_steps 1875 \
+    --eval_steps 187 \
+    --learning_rate 0.0002 \
+    --adam_beta2 0.999 \
+    --max_grad_norm 0.3 \
+    --lora_dropout 0.1 \
+    --weight_decay 0.0 \
+    --seed 0 \
+    >guanaco_7b_nods_2gpu.log 2>&1 &
