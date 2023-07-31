cc1f4ceeadf1eae6234ece1f334e43581fe97626
Eric Hartford <ehartford@gmail.com>
2023-07-30 23:01:07 -0700

    fix

--- patch_flash_attn.py
+++ patch_flash_attn.py
@@ -1,16 +1,31 @@
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
 import torch
-
+from torch import nn
+import warnings
 import transformers
 from transformers.models.llama.modeling_llama import apply_rotary_pos_emb
 
-from einops import rearrange
+try:
+    from flash_attn.flash_attn_interface import flash_attn_varlen_qkvpacked_func
+    from flash_attn.bert_padding import unpad_input, pad_input
+except Exception:
+    raise ModuleNotFoundError(
+        "Please install FlashAttention first, e.g., with pip install flash-attn --no-build-isolation, Learn more at https://github.com/Dao-AILab/flash-attention#installation-and-features"
+    )
 
-from flash_attn import flash_attn_varlen_qkvpacked_func
-from flash_attn.bert_padding import unpad_input, pad_input
+try:
+    from einops import rearrange
+except Exception:
+    raise ModuleNotFoundError(
+        "Please install einops first, e.g., with pip install einops"
+    )
 
 
+# ADAPTED from https://github.com/allenai/open-instruct/blob/main/open_instruct/llama_flash_attn_monkey_patch.py
+# AND https://github.com/lm-sys/FastChat/blob/main/fastchat/train/llama_flash_attn_monkey_patch.py
+# AND https://github.com/LAION-AI/Open-Assistant/blob/04fa9a24b2a58c8885b8aa6a2eb02b18de6b4961/model/model_training/models/patching_llama.py
+# AND Sourabh https://github.com/huggingface/transformers/commit/ee81bf5aee0d65f005d157c013777e3d27d8d6bf
 def forward(
     self,
     hidden_states: torch.Tensor,
@@ -24,6 +39,11 @@ def forward(
 
     attention_mask: [bsz, q_len]
     """
+    if output_attentions:
+        warnings.warn(
+            "Output attentions is not supported for patched `LlamaAttention`, returning `None` instead."
+        )
+
     bsz, q_len, _ = hidden_states.size()
 
     query_states = (
@@ -45,15 +65,32 @@ def forward(
     # [bsz, nh, q_len, hd]
 
     kv_seq_len = key_states.shape[-2]
-    assert past_key_value is None, "past_key_value is not supported"
-
+    if past_key_value is not None:
+        kv_seq_len += past_key_value[0].shape[-2]
     cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
     query_states, key_states = apply_rotary_pos_emb(
         query_states, key_states, cos, sin, position_ids
     )
-    # [bsz, nh, t, hd]
-    assert not output_attentions, "output_attentions is not supported"
-    assert not use_cache, "use_cache is not supported"
+
+    # Past Key value support
+    if past_key_value is not None:
+        # reuse k, v, self_attention
+        key_states = torch.cat([past_key_value[0], key_states], dim=2)
+        value_states = torch.cat([past_key_value[1], value_states], dim=2)
+
+    past_key_value = (key_states, value_states) if use_cache else None
+
+    # PEFT Int4 support
+    query_states, key_states, value_states = [
+        x.to(torch.bfloat16) for x in [query_states, key_states, value_states]
+    ]
+
+    assert all(
+        (
+            i.dtype in [torch.float16, torch.bfloat16]
+            for i in (query_states, key_states, value_states)
+        )
+    ), "shit not all types"
 
     # Flash attention codes from
     # https://github.com/HazyResearch/flash-attention/blob/main/flash_attn/flash_attention.py
@@ -89,13 +126,12 @@ def forward(
         )
         output = rearrange(
             pad_input(
-                rearrange(output_unpad,
-                          "nnz h d -> nnz (h d)"), indices, bsz, q_len
+                rearrange(output_unpad, "nnz h d -> nnz (h d)"), indices, bsz, q_len
             ),
             "b s (h d) -> b s h d",
             h=nheads,
         )
-    return self.o_proj(rearrange(output, "b s h d -> b s (h d)")), None, None
+    return self.o_proj(rearrange(output, "b s h d -> b s (h d)")), None, past_key_value
 
 
 # Disable the transformation of the attention mask in LlamaModel as the flash attention
@@ -107,8 +143,22 @@ def _prepare_decoder_attention_mask(
     return attention_mask
 
 
-def replace_llama_attn_with_flash_attn():
+def replace_attn_with_flash_attn():
+    cuda_major, cuda_minor = torch.cuda.get_device_capability()
+    if cuda_major < 8:
+        print(
+            "Flash attention is only supported on Ampere or Hopper GPU during training due to head dim > 64 backward."
+            "ref: https://github.com/HazyResearch/flash-attention/issues/190#issuecomment-1523359593"
+        )
     transformers.models.llama.modeling_llama.LlamaModel._prepare_decoder_attention_mask = (
         _prepare_decoder_attention_mask
     )
     transformers.models.llama.modeling_llama.LlamaAttention.forward = forward
+
+
+def unplace_flash_attn_with_attn():
+    import importlib
+    import transformers
+
+    print("Reloading llama model, unpatching flash attention")
+    importlib.reload(transformers.models.llama.modeling_llama)
